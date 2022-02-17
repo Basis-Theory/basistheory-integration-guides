66ffde6d0a94e362d3c4f89e07e56679c4c9a9f0
Brian Gonzalez <brian.gonzalez@basistheory.com>
2022-02-17 08:16:13 -0700

    chore: looks for sidebar category in pathname instead of href

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     sidebarClassCategories.forEach((sidebarClassCategory) => {
-        if (document.location.href.includes(sidebarClassCategory) || document.location.pathname === '/') {
+        if (document.location.pathname.includes(sidebarClassCategory) || document.location.pathname === '/') {
             const categoryExpander = document.querySelector(`#${sidebarClassCategory}-category-expander`);
             const categoryItems = document.querySelector(`#${sidebarClassCategory}-category-items`);
 
